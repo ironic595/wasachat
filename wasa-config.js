57387f5c952077669e8ac58f3284794e2f68628b
ironic595 <ironic595@gmail.com>
2026-08-25 17:32:17 -0300

    Create wasa-config.js

---
+++ wasa-config.js
@@ -0,0 +1,11 @@
+// wasa.chat - Config centralizada
+// Cambiá SOLO esta URL cada vez que deployás nuevo Apps Script y se actualiza todo el sitio
+const WASA_WEBHOOK_URL = 'https://script.google.com/macros/s/AKfycbzE0jRCJvfLo4xdKfNvrejtLH7yU_mnyewdxCDSH7EDeqvm1nBvdrHXcKoAs0WlFL2-/exec';
+
+// Compatibilidad con código viejo que usa WEBHOOK_URL o SHEET_WEBHOOK_URL
+const WEBHOOK_URL = WASA_WEBHOOK_URL;
+const SHEET_WEBHOOK_URL = WASA_WEBHOOK_URL;
+const BASE_CONFIG = { SHEET_WEBHOOK_URL: WASA_WEBHOOK_URL, CHAIN_ID: 56 };
+
+// Opcional: log para debug
+console.log('WASA Config cargada:', WASA_WEBHOOK_URL.slice(-20));
