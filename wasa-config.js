20acff3785cec9ff97647277cf8367385be7cfb5
ironic595 <ironic595@gmail.com>
2026-08-26 17:12:42 -0300

    Update WASA_WORKER_KEY in wasa-config.js

--- wasa-config.js
+++ wasa-config.js
@@ -1,6 +1,6 @@
 // wasa-config.js CON CLAVE Y CACHE BUSTER
 const WASA_WORKER_URL = 'https://founderswasablocks.javimsites.workers.dev';
-const WASA_WORKER_KEY = 'wasa_blocks_f0und3rs_uidf8923hryuwe3987'; // CAMBIA ESTO
+const WASA_WORKER_KEY = 'wasa_2026_f0und3rs_9k2j4l8x'; // CAMBIA ESTO
 const WASA_VERSION = 'v' + Date.now(); // cache buster automatico
 
 const WEBHOOK_URL = WASA_WEBHOOK_URL;
